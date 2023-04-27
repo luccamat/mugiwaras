9a9ff2058bc46d19a2f3f51a2ce7628e90903861
Lucca Matheus <74078934+luccamat@users.noreply.github.com>
2023-04-27 11:46:43 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 //Swiper slider
 var swiper = new Swiper(".bg-slider-thumbs", {
     loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
   var swiper2 = new Swiper(".bg-slider", {
     loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
+    spaceBetween: 0,
+    thumbs:{
       swiper: swiper,
     },
-  });
+});
+
 //Navigation bar effects on scroll
+
+window.addEventListener("scroll", function(){
+  const header =this.document.querySelector("header");
+  header.classList.toggle("sticky",window.scrollY > 0);
+});
 
 //responsive navigation menu toggle
